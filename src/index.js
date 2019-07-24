3120e7d6c1199227a1989570c9b0dfaa0b403fb0
felixsonyusuftosin <felixsonyusuftosin@gmail.com>
2019-07-24 21:29:38 +0100

    remove require babel pollyfill

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ require("babel-register")({
     presets: ["env"],
 });
 require("babel-core/register");
-
 // Import the rest of our application.
 
 const { setUpCombinedReducers } = require("./reducers");
